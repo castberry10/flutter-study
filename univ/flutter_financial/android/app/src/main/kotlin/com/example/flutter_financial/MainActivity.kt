88d740556d36f4221039786b9877e4a43afb4c83
wychoi <soc09072@gmail.com>
2024-10-08 11:05:57 +0900

    Financial(view)앱 init

---
+++ univ/flutter_financial/android/app/src/main/kotlin/com/example/flutter_financial/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_financial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ univ/flutter_financial/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Financial());
+}
+
+class Financial extends StatelessWidget {
+  const Financial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
