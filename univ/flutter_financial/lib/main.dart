e1e850c2ad6559af1481a0297cd390c0cbe3a7ee
wychoi <soc09072@gmail.com>
2024-10-08 11:31:43 +0900

    Financeial 앱 .. 1

--- univ/flutter_financial/lib/main.dart
+++ univ/flutter_financial/lib/main.dart
@@ -9,6 +9,46 @@ class Financial extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+        home: Scaffold(
+      backgroundColor: Color(0xFF181818),
+      body: Center(
+        child: Text(
+          "abcdefedfad",
+          style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+        ),
+      ),
+    ));
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //       home: Scaffold(
+  //     backgroundColor: const Color(0xFF181818),
+  //     body: SingleChildScrollView(
+  //       child: Padding(
+  //         padding: const EdgeInsets.symmetric(horizontal: 20),
+  //         child: Column(
+  //           children: [
+  //             const SizedBox(),
+  //             Row(),
+  //             const SizedBox(),
+  //             Text(),
+  //             const SizedBox(),
+  //             const Text(),
+  //             const SizedBox(),
+  //             Row(),
+  //             const SizedBox(),
+  //             Row(),
+  //             const SizedBox(),
+  //             Container(),
+  //             Transform.translate(),
+  //             Transform.translate(),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   ));
+  // }
 }
