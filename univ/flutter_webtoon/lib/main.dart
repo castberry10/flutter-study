927a652fdf27e5ba6681f110eee59c83f85c5b2f
wychoi <soc09072@gmail.com>
2024-11-05 21:03:08 +0900

    홈스크린 maindart에 적용

--- univ/flutter_webtoon/lib/main.dart
+++ univ/flutter_webtoon/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webtoon/screens/home_screen.dart';
 
 void main() {
   runApp(App());
@@ -9,6 +10,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
