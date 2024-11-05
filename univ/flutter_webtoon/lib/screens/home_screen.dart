f7f06871f8ed859abaf24c6bb8dcfe2c80955e28
wychoi <soc09072@gmail.com>
2024-11-05 22:58:50 +0900

    다시 스테이트레스 위젯

--- univ/flutter_webtoon/lib/screens/home_screen.dart
+++ univ/flutter_webtoon/lib/screens/home_screen.dart
@@ -1,33 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webtoon/models/webtoon_model.dart';
-import 'package:flutter_webtoon/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-  void waitForWebtoons() async {
-    webtoons = await ApiService.getTodayToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebtoons();
-  }
-
+  
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
