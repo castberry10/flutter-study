5e610296a8b69f9dd0603c933778facbf6aae391
wychoi <soc09072@gmail.com>
2024-11-05 23:44:49 +0900

    이미지 추가

--- univ/flutter_webtoon/lib/screens/home_screen.dart
+++ univ/flutter_webtoon/lib/screens/home_screen.dart
@@ -38,16 +38,48 @@ class HomeScreen extends StatelessWidget {
 
   ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
     return ListView.separated(
-            scrollDirection: Axis.horizontal,
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) {
-              print(index);
-              var webtoon = snapshot.data![index];
-              return Text(webtoon.title);
-            },
-            separatorBuilder: (context, index) => const SizedBox(
-              width: 20,
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 10,
+                        offset: const Offset(10, 10),
+                        color: Colors.black.withOpacity(0.5))
+                  ]),
+              width: 250,
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit 537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari 537.36"
+                },
+              ),
             ),
-          );
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 20,
+      ),
+    );
   }
 }
