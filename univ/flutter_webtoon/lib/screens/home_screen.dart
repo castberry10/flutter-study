aa63749d2515989f46d752e89b8c969f86fe9295
wychoi <soc09072@gmail.com>
2024-11-05 23:52:19 +0900

    Fix User-Agent header formatting in HomeScreen

--- univ/flutter_webtoon/lib/screens/home_screen.dart
+++ univ/flutter_webtoon/lib/screens/home_screen.dart
@@ -60,7 +60,7 @@ class HomeScreen extends StatelessWidget {
                 webtoon.thumb,
                 headers: const {
                   "User-Agent":
-                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit 537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari 537.36"
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
                 },
               ),
             ),
