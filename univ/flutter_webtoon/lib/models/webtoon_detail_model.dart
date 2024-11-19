11c3a061784c8c030f18baabf78280b3f62adfce
wychoi <soc09072@gmail.com>
2024-11-19 11:05:17 +0900

    Add WebtoonDetailModel and WebtoonEpisodeModel for JSON parsing

---
+++ univ/flutter_webtoon/lib/models/webtoon_detail_model.dart
@@ -0,0 +1,18 @@
+class WebtoonDetailModel {
+  final String title, about, genre, age;
+  WebtoonDetailModel.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        about = json['about'],
+        genre = json['genre'],
+        age = json['age'];
+}
+
+class WebtoonEpisodeModel {
+  final String id, title, rating, date;
+
+  WebtoonEpisodeModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        rating = json['rating'],
+        date = json['data'];
+}
