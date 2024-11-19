b747b9c2b8b0acb5f5159f7157d8a94bb4e4045f
wychoi <soc09072@gmail.com>
2024-11-19 11:15:11 +0900

    Add API methods for fetching webtoon details and latest episodes

--- univ/flutter_webtoon/lib/services/api_service.dart
+++ univ/flutter_webtoon/lib/services/api_service.dart
@@ -1,6 +1,8 @@
+import 'package:flutter_webtoon/models/webtoon_detail_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_webtoon/models/webtoon_model.dart';
+import 'package:f';
 
 class ApiService {
   static String baseUrl = "https://tinyurl.com/36hmb9af";
@@ -22,5 +24,30 @@ class ApiService {
       print(e.toString());
     }
     return webtoonInstances;
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseUrl/$id');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodeInstances = [];
+    final url = Uri.parse('$baseUrl/$id');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodeInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodeInstances;
+    }
+    throw Error();
   }
 }
