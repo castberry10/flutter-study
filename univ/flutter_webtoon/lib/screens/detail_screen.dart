3dbc1b9948b3de97a79683009d85f06229e28b53
wychoi <soc09072@gmail.com>
2024-11-19 10:54:32 +0900

    Add image display and user-agent handling in DetailScreen

--- univ/flutter_webtoon/lib/screens/detail_screen.dart
+++ univ/flutter_webtoon/lib/screens/detail_screen.dart
@@ -26,6 +26,38 @@ class DetailScreen extends StatelessWidget {
           ),
         ),
       ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 10,
+                          offset: const Offset(10, 10),
+                          color: Colors.black.withOpacity(0.5))
+                    ]),
+                width: 250,
+                child: Image.network(
+                  thumb,
+                  headers: const {
+                    "User-Agent":
+                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                    "Referer": "https://comic.naver.com",
+                  },
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
