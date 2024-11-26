defdcd93047154b4f65f4c3eff5426cdeafd1fae
wychoi <soc09072@gmail.com>
2024-11-26 10:23:26 +0900

    onHeartTap 구현 - state 반전 - SharedPreferences에 저장

--- univ/flutter_webtoon/lib/screens/detail_screen.dart
+++ univ/flutter_webtoon/lib/screens/detail_screen.dart
@@ -44,6 +44,21 @@ class _DetailScreenState extends State<DetailScreen> {
     initPrefs();
   }
 
+  onHeartTap() async {
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.id);
+      } else {
+        likedToons.add(widget.id);
+      }
+      await prefs.setStringList('likedToons', likedToons);
+      setState(() {
+        isLiked = !isLiked;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,7 +70,9 @@ class _DetailScreenState extends State<DetailScreen> {
         actions: [
           IconButton(
             onPressed: () {},
-            icon: const Icon(Icons.favorite_outline),
+            icon: Icon(
+              isLiked ? Icons.favorite : Icons.favorite_outline,
+            ),
           ),
         ],
         title: Text(
