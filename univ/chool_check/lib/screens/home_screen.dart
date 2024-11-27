feb63fa51f8139c9310d4542170dc2afe6dbb700
wychoi <soc09072@gmail.com>
2024-11-27 10:31:28 +0900

    위치 권한 관리 구현

--- univ/chool_check/lib/screens/home_screen.dart
+++ univ/chool_check/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomeScreen extends StatelessWidget {
   static const LatLng univLatLng = LatLng(
@@ -13,9 +14,9 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: renderAppbar(),
-      body: const Column(
+      body: Column(
         children: [
-          Expanded(
+          const Expanded(
             flex: 2,
             child: GoogleMap(
               initialCameraPosition: CameraPosition(
@@ -24,6 +25,24 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ),
+          Expanded(
+            child: Column(
+              children: [
+                const Icon(
+                  Icons.timelapse_outlined,
+                  color: Colors.blue,
+                  size: 50,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('출근하기'),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
@@ -41,5 +60,28 @@ class HomeScreen extends StatelessWidget {
       ),
       backgroundColor: Colors.white,
     );
+  }
+
+  Future<String> checkPermission() async {
+    final isLocationEnbled =
+        await Geolocator.isLocationServiceEnabled(); // 위치서비스 활성화 여부
+    if (!isLocationEnbled) {
+      return '위치 서비스 활성화 해주세요';
+    }
+    LocationPermission checkedPermission =
+        await Geolocator.checkPermission(); // 위치 권한 확인
+    if (checkedPermission == LocationPermission.denied) {
+      // 권한 없으면
+      checkedPermission = await Geolocator.requestPermission(); // 요청
+      if (checkedPermission == LocationPermission.denied) {
+        // 거절되면
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+    // 앱에서 재요청 불가한 거절
+    if (checkedPermission == LocationPermission.deniedForever) {
+      return '앱의 위치 권한을 설정에서 허가해주세요';
+    }
+    return '위치 권한이 허가되었습니다.';
   }
 }
