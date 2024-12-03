0a248d87e8693a5fb35a5013a7e76a533443ef5f
wychoi <soc09072@gmail.com>
2024-12-03 11:38:21 +0900

    home_screen.dart 1. StatefulWidget으로 변경 2. 선택된 날짜를 관리할 변수 추가 3. 선택된 날짜 전달(UI 반영) 4. 날짜가 선택됐을 때 실행할 함수 추가

--- univ/flutter_calendar_scheduler/lib/screens/home_screen.dart
+++ univ/flutter_calendar_scheduler/lib/screens/home_screen.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_scheduler/components/main_calendar.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Column(
-          children: [MainCalendar()],
+          children: [
+            MainCalendar(
+              selectedDate: selectedDate,
+              onDaySelected: onDaySelected,
+            )
+          ],
         ),
       ),
     );
+  }
+
+  void onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      selectedDate = selectedDay;
+    });
   }
 }
