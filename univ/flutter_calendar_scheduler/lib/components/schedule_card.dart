ed55e1731097e7de33b77ae3423a15a37a8ed30c
wychoi <soc09072@gmail.com>
2024-12-03 16:34:29 +0900

    § 스케줄 카드 구현 1. ScheduleCard 클래스 생성 2. 멤버 변수 추가 3. 생성자 수정 4. Container 추가 5. IntrinsicHeight 추가 6. Row 추가 7. 시작과 종료일정을 보여주는 위젯 추가 8. 일정(내용) 을 보여주는 위젯 추가

--- univ/flutter_calendar_scheduler/lib/components/schedule_card.dart
+++ univ/flutter_calendar_scheduler/lib/components/schedule_card.dart
@@ -1,6 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_scheduler/const/colors.dart';
 
+class ScheduleCard extends StatelessWidget {
+  final int startTime;
+  final int endTime;
+  final String content;
+
+  const ScheduleCard({
+    super.key,
+    required this.startTime,
+    required this.endTime,
+    required this.content,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1.0,
+          color: PRIMARY_COLOR,
+        ),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: IntrinsicHeight(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _Time(startTime: startTime, endTime: endTime),
+              const SizedBox(width: 16),
+              _Content(content: content),
+              const SizedBox(width: 16),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class _Time extends StatelessWidget {
   final int startTime;
   final int endTime;
