1625efc2d1454144e28e31433be5c89e0a0ef1bf
wychoi <soc09072@gmail.com>
2024-12-03 11:11:46 +0900

    홈스크린에 캘린더 연결

--- univ/flutter_calendar_scheduler/lib/screens/home_screen.dart
+++ univ/flutter_calendar_scheduler/lib/screens/home_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calendar_scheduler/components/main_calendar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [MainCalendar()],
+        ),
+      ),
+    );
   }
 }
