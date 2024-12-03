8e160f629eaf87cbb63b0e5032c3560f62d11f03
wychoi <soc09072@gmail.com>
2024-12-03 11:06:22 +0900

    메인 캘린더

---
+++ univ/flutter_calendar_scheduler/lib/components/main_calendar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class MainCalendar extends StatelessWidget {
+  const MainCalendar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      focusedDay: DateTime.now(),
+      firstDay: DateTime(1900, 1, 1),
+      lastDay: DateTime(2999, 12, 31),
+    );
+  }
+}
