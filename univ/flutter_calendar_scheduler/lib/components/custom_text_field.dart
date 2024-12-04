32febc99a9b94b3f39025413ae0c224b0a720eac
wychoi <soc09072@gmail.com>
2024-12-04 10:37:49 +0900

    텍스트 필드 위젯 변경 1. 시간을 입력하는 텍스트 필드인지 확인하 는 변수 추가 2. 생성자 추가 3. 텍스트 필드 스타일링

--- univ/flutter_calendar_scheduler/lib/components/custom_text_field.dart
+++ univ/flutter_calendar_scheduler/lib/components/custom_text_field.dart
@@ -1,32 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_calendar_scheduler/const/colors.dart';
 
-class CustomTextField extends StatefulWidget {
+class CustomTextField extends StatelessWidget {
   final String label;
-
+  final bool isTime;
   const CustomTextField({
     required this.label,
+    required this.isTime,
     super.key,
   });
 
-  @override
-  State<CustomTextField> createState() => _CustomTextFieldState();
-}
-
-class _CustomTextFieldState extends State<CustomTextField> {
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          widget.label,
+          label,
           style: const TextStyle(
             color: PRIMARY_COLOR,
             fontWeight: FontWeight.w600,
           ),
         ),
-        TextFormField(),
+        Expanded(
+          flex: isTime ? 0 : 1,
+          child: TextFormField(
+            cursorColor: Colors.grey, // 커서 색상
+            maxLines: isTime ? 1 : null,
+            expands: !isTime,
+            keyboardType:
+                isTime ? TextInputType.number : TextInputType.multiline,
+            inputFormatters:
+                isTime ? [FilteringTextInputFormatter.digitsOnly] : [],
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              filled: true,
+              fillColor: Colors.grey[300],
+              suffixText: isTime ? '시' : null,
+            ),
+          ),
+        ),
       ],
     );
   }
