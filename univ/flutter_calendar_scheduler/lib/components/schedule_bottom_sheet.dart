be12a80246d9c1e4895878b70aae43c003557411
wychoi <soc09072@gmail.com>
2024-12-04 10:48:11 +0900

    CustomTextField 위젯 반영

--- univ/flutter_calendar_scheduler/lib/components/schedule_bottom_sheet.dart
+++ univ/flutter_calendar_scheduler/lib/components/schedule_bottom_sheet.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_scheduler/components/custom_text_field.dart';
+import 'package:flutter_calendar_scheduler/const/colors.dart';
+
 class ScheduleBottomSheet extends StatefulWidget {
   const ScheduleBottomSheet({super.key});
 
@@ -14,7 +17,50 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
       child: Container(
         height: MediaQuery.of(context).size.height / 2,
         color: Colors.white,
-        child: const CustomTextField(label: '시작 시간'),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8, right: 8),
+          child: Column(
+            children: [
+              const Row(
+                children: [
+                  Expanded(
+                    child: CustomTextField(
+                      label: '시작 시간',
+                      isTime: true,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: CustomTextField(
+                      label: '종료 시간',
+                      isTime: true,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              const Expanded(
+                child: CustomTextField(
+                  label: '내용',
+                  isTime: false,
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: PRIMARY_COLOR),
+                  child: const Text('저장'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
